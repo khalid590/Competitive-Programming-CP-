3f9495cbaad2365bc267240151d277e2504d3118
khalid590 <75054950+khalid590@users.noreply.github.com>
2020-11-26 22:40:51 +0600

    Add files via upload

---
+++ 1004_Simple Product.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,result;
+
+    cin>> a >>b;
+
+    result = a*b;
+    cout<< "PROD = " <<result <<endl;
+
+    return 0;
+}
